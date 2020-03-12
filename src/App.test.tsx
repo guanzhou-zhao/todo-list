257261549f21b8a3953535b99441a79e5d0219e8
Guanzhou Zhao <benzhao2866@gmail.com>
2020-03-12 23:22:47 +1300

    test for to do list

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { unmountComponentAtNode } from 'react-dom';
 import { render } from '@testing-library/react';
 import App from './App';
 
+
+let container = null;
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+})
+
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/to do list/i);
   expect(linkElement).toBeInTheDocument();
 });
